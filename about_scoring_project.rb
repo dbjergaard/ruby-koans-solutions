91421c744bfc155164175bb6e50a2fda79b612bd
David Bjergaard <dbjergaard@gmail.com>
2015-02-13 13:45:37 -0500

    Scoring exercise changes

--- about_scoring_project.rb
+++ about_scoring_project.rb
@@ -29,33 +29,70 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 #
 # Your goal is to write the score method.
 
+# shamelessly stolen from:
+# https://github.com/hcf/99-problems-in-Ruby/blob/master/99_problems.rb
+def pack_dups(input)
+  input.inject([[]]) do |list, atom|
+    if list[-1][-1] == atom or list[-1][-1].nil?
+      list[-1] << atom
+    else
+      list << [atom]
+    end
+    list
+  end
+end
+
 def score(dice)
   # You need to write this method
-
+  if dice.empty?
+    return 0
+  end
   # sort the list of dice
   dice.sort!
   # slice the list according to repeated dice
-  
-  # shamelessly stolen from:
-  # https://github.com/hcf/99-problems-in-Ruby/blob/master/99_problems.rb
-  sliced = dice.inject([[]]) do |array, current| 
-    if array[-1][-1] == current or array[-1][-1].nil?
-      array[-1] << current
-    else
-      array << [current]
+  # puts dice
+  sliced = pack_dups(dice)
+  value = 0
+  sliced.each do |group| 
+    if group.size > 3
+      if group[0] == 1
+        value += 1000
+        value += 100*(group.size - 3)
+      else
+        
+      end
+    end
+    if group.size == 3
+      if group[0] == 1
+        value += 1000
+      else
+        value += 100*group[0]
+      end
+    elsif group.size == 2
+      if group[0] == 5
+        value += 100
+      elsif group[0] == 1
+        value += 200
+      end
+    elsif group.size == 1
+      if group[0] == 5
+        value += 50
+      elsif group[0] == 1
+        value += 100
+      end
     end
   end
-  puts sliced
+  value
   # determine score from length of sub lists
 end
 
 class AboutScoringProject < Neo::Koan
   def test_score_of_an_empty_list_is_zero
-    score([1,1,1,2,2,2,3,3,3])
     assert_equal 0, score([])
   end
 
   def test_score_of_a_single_roll_of_5_is_50
+    score([1,1,1,2,2,2,3,3,3])
     assert_equal 50, score([5])
   end
 
